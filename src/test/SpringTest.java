9817d251b2b72e33d163be7b662d0a8838afc2fe
kayzhao <kay.zhao.233@gmail.com>
2016-12-22 10:34:18 +0800

    update the spring version(4.3.3.RELEASE) to match the Junit version(4.11)
    Signed-off-by: kayzhao <kay.zhao.233@gmail.com>

--- src/test/SpringTest.java
+++ src/test/SpringTest.java
@@ -1,4 +1,3 @@
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -6,7 +5,8 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:spring/root-context.xml" })
+@ContextConfiguration(locations = { "classpath*:spring/root-context.xml",
+		"classpath*:spring/applicationContext-mvc.xml.xml" })
 public class SpringTest extends AbstractJUnit4SpringContextTests {
 
 	public <T> T getBean(Class<T> type) {
