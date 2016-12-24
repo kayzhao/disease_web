4a7398a9803b0df211afbc82a827b89009fc8939
kayzhao <kay.zhao.233@gmail.com>
2016-12-24 20:19:56 +0800

    change the test use case

--- src/test/java/umls/UMLSTest.java
+++ src/test/java/umls/UMLSTest.java
@@ -3,23 +3,29 @@ package umls;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import base.SpringTest;
 
 import com.csu.bio.common.constants.PropertyConfigurer;
 import com.csu.bio.util.UMLSProps;
+import com.csu.bio.util.web.MyApplicationContext;
 
 /**
  *
  * @author kayzhao
  * @version
  */
-public class UMLSTest extends SpringTest {
 
-	
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:/spring/applicationContext-props.xml" })
+public class UMLSTest {
+
 	@Test
 	public void test() throws Exception {
-		UMLSProps umlsProps = (UMLSProps) getBean("umlsProps");
+		UMLSProps umlsProps = (UMLSProps) MyApplicationContext.getBean(UMLSProps.class);
 
 		System.out.println("--- load property ----");
 		System.out.println(umlsProps.toString());
